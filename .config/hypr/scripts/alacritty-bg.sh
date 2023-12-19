ab0483d58cae5412f4308cd5c084cc6cc857f0f0
Avraham <avrahambenaramsouza@gmail.com>
2023-12-19 15:17:11 -0300

    feat: closing alacritty-bg

--- .config/hypr/scripts/alacritty-bg.sh
+++ .config/hypr/scripts/alacritty-bg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-entries="Cava Cmatrix Pipes"
+entries="Cava Cmatrix Pipes Close"
 
 selected=$(printf '%s\n' $entries | wofi --conf=$HOME/.config/wofi/config.power --style=$HOME/.config/wofi/style.widgets.css | awk '{print tolower($1)}')
 
@@ -11,4 +11,7 @@ case $selected in
 	alacritty --class="alacritty-bg" -e ~/.config/hypr/cmatrix.sh;;
   pipes)
 	alacritty --class="alacritty-bg" -e ~/.config/hypr/pipes.sh;;
+  close)
+	pid=$(python3 ~/.config/hypr/scripts/alacritty-kill.py)
+	kill $pid
 esac
