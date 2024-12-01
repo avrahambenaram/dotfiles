aa596bf2048017e7be8fd108a989c89549d4759b
Avraham <souzaavraham@proton.me>
2024-11-30 21:17:06 -0300

    feat(nvim): autocmd for formatting go files

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -49,6 +49,13 @@ return {
       }
       lspconfig.tailwindcss.setup {}
       lspconfig.ts_ls.setup {}
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
     end,
   },
 
