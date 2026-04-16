455df79977a0e8608fac50b2f80a4091cf01e722
Avraham <souzaavraham@proton.me>
2026-04-16 14:46:12 -0300

    chore: remove nvim-treesitter and setup folding

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -34,3 +34,47 @@ vim.o.termguicolors = true
 vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("n", "<C-y>", '"+y', { desc = "Yank to system clipboard" })
 vim.keymap.set("v", "<C-y>", '"+y', { desc = "Yank to system clipboard" })
+
+-- Folding
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.fillchars = {
+  fold = " ",
+  foldopen = "",
+  foldclose = "",
+  foldsep = " ",
+}
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 99
+vim.opt.foldenable = true
+function _G.custom_foldtext()
+  local start_line = vim.fn.getline(vim.v.foldstart)
+  local line_count = vim.v.foldend - vim.v.foldstart + 1
+
+  return string.format("󰁂 %s  (%d lines)", start_line:gsub("^%s*", ""), line_count)
+end
+
+vim.opt.foldtext = "v:lua.custom_foldtext()"
+
+-- Treesitter
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  callback = function(args)
+    local buf = args.buf
+    local ft = vim.bo[buf].filetype
+
+    if ft == "" then
+      return
+    end
+
+    local lang = vim.treesitter.language.get_lang(ft) or ft
+
+    pcall(vim.treesitter.start, buf, lang)
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    vim.wo.foldmethod = "expr"
+    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  end,
+})
